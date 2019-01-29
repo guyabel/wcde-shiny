8f3eb4566674ecf8f0f4eca59acc4bbc18083399
Guy Abel <g.j.abel@gmail.com>
2019-01-29 18:48:08 +0800

    tidy

--- run.R
+++ run.R
@@ -1,28 +1,9 @@
-#setwd("C:/Users/gabel/Documents/")
-#setwd("C:/Users/Guy/Documents/Github")
-# setwd("E:/VID/project/")
 library(shiny)
 runApp(launch.browser = TRUE)
 
 
-library(profvis)
-profvis({
-  runApp(launch.browser = TRUE)
-})
+# library(profvis)
+# profvis({
+#   runApp(launch.browser = TRUE)
+# })
 
-
-#runApp("./shiny/wcde")
-#shiny::runGitHub('wicpyr', 'gjabel')
-#setwd("./shiny/wcde")
-#setwd("C:/Users/gabel/Documents/")
-
-#runApp("./shiny/shiny-partials-master")
-
-#shiny::runGitHub('wicpyr', 'gjabel')
-# runGitHub("shiny-examples", "rstudio", subdir = "050-kmeans-example")
-# runGitHub("shiny-examples", "rstudio", subdir = "052-navbar-example")
-# runGitHub("shiny-examples", "rstudio", subdir = "048-including-html-text-and-markdown-files")
-# 
-# runGitHub("shiny_example", "rstudio")
-# 
-# runExample("02_text")
